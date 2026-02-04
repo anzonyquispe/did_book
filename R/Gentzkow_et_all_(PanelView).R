1a3a6d32768bb46fde058c84335e69ec36043d9e
anthony-03-pe <quisperojasa@udesa.edu.ar>
2026-02-04 18:52:34 -0500

    Update Gentzkow_et_all_(PanelView).R

--- R/Gentzkow_et_all_(PanelView).R
+++ R/Gentzkow_et_all_(PanelView).R
@@ -36,3 +36,26 @@ panelview(prestout ~ same_treat_after_first_change, data = datos100,
           main = "Treatment Status: Gentzkow et al. (2011)",
           cex.main = 16, cex.axis.x = 8, cex.axis.y = 5,
           cex.lab = 16, cex.legend = 12)
+library(dplyr)
+
+#Create the treatments's variable that capture the timing
+datos <- datos %>%
+  mutate(treat_timing = ifelse(first_change >= 1e+06, 0,
+                               ifelse(year >= first_change, 1, 0)))
+
+# Verify
+datos %>% 
+  group_by(cnty90) %>%
+  summarise(first = min(year[treat_timing == 1], na.rm = TRUE)) %>%
+  pull(first) %>% table()
+
+# Graph
+panelview(data = datos, Y = "prestout", D = "treat_timing",
+          index = c("cnty90", "year"),
+          by.timing = TRUE, display.all = TRUE,
+          type = "outcome", by.cohort = TRUE,
+          xlab = "Year", ylab = "Presidential Turnout",
+          main = "Average Outcomes by Cohort: Gentzkow et al. (2011)",
+          cex.main = 16, cex.axis = 10, cex.lab = 14, cex.legend = 12,
+          axis.lab.gap = c(4, 0),
+          lwd = 1.5)
